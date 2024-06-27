4d72b4079ec3cea60d85cc6f31142a3f3dfc19d2
hemantlunia <149136418+hemantlunia@users.noreply.github.com>
2024-06-27 14:12:51 +0530

    Complete

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useRef, useState } from 'react'
+import TodoList from './TodoList';
+import bookss from "../icon/book.png"
+import toast from 'react-hot-toast'
+
+function TodoApp() {
+
+  const [newT,setNewt] = useState(localStorage.getItem("notes")?JSON.parse(localStorage.getItem("notes")):[])
+  const [toggleItem,setToggleItem] = useState(null)
+
+  useEffect(()=>{
+    localStorage.setItem("notes",JSON.stringify(newT))
+  },[newT])
+  
+  const inputRef = useRef()
+  const add = ()=>{
+        const text = inputRef.current.value.trim();
+        if (text.length <= 0) {
+          return null;
+        }
+        const newtodo = {
+          id: Date.now(),
+          todo: text,
+          isComplete:false
+        }
+        
+        setNewt((prev)=>[newtodo,...prev])
+        inputRef.current.value = ''
+        toast("Note Added Successfully!",{
+          duration:1500,
+          position:'top-right',
+          style:{
+            backgroundColor:"green",
+            color:"white",
+            borderRadius:"40px"
+          }
+        })
+      
+  }
+  const deletebtn = (id)=>{
+            setNewt((prev)=>prev.filter((todo)=>todo.id!==id))
+            toast("Selected Note Deleted Successfully!",{
+              duration:1500,
+              position:'top-right',
+              style:{
+                backgroundColor:"red",
+                color:"white",
+                borderRadius:"40px"
+              }
+            })
+  }
+  
+  const toggle =(id)=>{
+    toast("Toggle Successfully!",{
+      duration:1500,
+      position:'top-right',
+      style:{
+        backgroundColor:"gold",
+        color:"white",
+        borderRadius:"40px"
+      }
+    })
+          setNewt((prev)=>prev.map((item)=>{
+           
+            if (item.id===id) {
+              const newStatus = !item.isComplete;
+            
+              return {...item,isComplete: newStatus}
+            }
+            return item;
+            
+          }))
+  }
+  return (
+    <>
+    
+       <div className='m-3'>
+
+        <div className='flex items-center mt-7 gap-2 ml-3'>
+               <img src={bookss} alt="BookPic" />
+               <h1 className='text-3xl font-bold'>Notes</h1>
+        </div>
+
+        <div className='flex items-center my-7 ml-3 bg-gray-200 rounded-full'>
+          <input className='ml-4 bg-transparent border-0 outline-none flex-1 h-14 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task...' ref={inputRef} />
+          <button className='border-none rounded-full bg-[navy] w-32 h-14 hover:text-[red] text-white text-lg font-medium cursor-pointer' onClick={add}>ADD +</button>
+        </div>
+      
+        <div className='grid grid-cols-3 gap-4'>
+          {newT.map((item,index)=>{
+            return <TodoList key={index} text={item.todo} deletebtn={deletebtn} id={item.id} isComplete={item.isComplete} toggle={toggle}/>
+          })}
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default TodoApp
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import checked from '../icon/checked.png'
+import deletes from '../icon/delete.png'
+import cancel from '../icon/cancel.png'
+
+function TodoList({ text, deletebtn, id, isComplete, toggle }) {
+  return (
+    <>
+
+
+
+<div class="max-w-sm m-4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-[purple] dark:border-[red] relative">
+    <p className={`mb-8 font-normal text-[white] break-words ${isComplete ? "line-through" : ""}`}>
+        {text}
+    </p>
+    <div class="absolute bottom-0 left-0 mb-4 ml-4">
+        <img
+            className="inline-flex items-center w-7 h-7"
+            onClick={() => deletebtn(id)}
+            src={deletes}
+            alt='Delete'
+        />
+    </div>
+    <div class="absolute bottom-0 right-0 mb-4 mr-4">
+        <img
+            className="inline-flex items-end font-medium bg-[navy] rounded-lg w-7 h-7"
+            onClick={() => toggle(id)}
+            src={isComplete ? cancel : checked}
+            alt='tik'
+        />
+    </div>
+</div>
+
+
+
+
+    </>
+  )
+}
+
+export default TodoList
