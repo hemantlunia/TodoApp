d3e2b2c54c01e4f226933c9554f1cb893bc4e4f4
Hemant Kumar <149136418+hemantlunia@users.noreply.github.com>
2025-02-24 15:30:17 +0530

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,11 +9,11 @@ function TodoList({ text, deletebtn, id, isComplete, toggle }) {
 
 
 
-<div class="max-w-sm m-4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-[purple] dark:border-[red] relative">
-    <p className={`mb-8 font-normal text-[white] break-words ${isComplete ? "line-through" : ""}`}>
+<div className="max-w-sm m-4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-[purple] dark:border-[red] relative">
+    <p className={`mb-8 font-normal text-black break-words ${isComplete ? "line-through" : ""}`}>
         {text}
     </p>
-    <div class="absolute bottom-0 left-0 mb-4 ml-4">
+    <div className="absolute bottom-0 left-0 mb-4 ml-4">
         <img
             className="inline-flex items-center w-7 h-7"
             onClick={() => deletebtn(id)}
@@ -21,7 +21,7 @@ function TodoList({ text, deletebtn, id, isComplete, toggle }) {
             alt='Delete'
         />
     </div>
-    <div class="absolute bottom-0 right-0 mb-4 mr-4">
+    <div className="absolute bottom-0 right-0 mb-4 mr-4">
         <img
             className="inline-flex items-end font-medium bg-[navy] rounded-lg w-7 h-7"
             onClick={() => toggle(id)}
